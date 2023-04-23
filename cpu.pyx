dfcc1777b3463cc76b610af11af5b233d4d3cf8c
crux <cruxdevy@gmail.com>
2023-04-23 19:18:01 +0530

    add CPU - LDA

--- cpu.pyx
+++ cpu.pyx
@@ -15,27 +15,53 @@ Emulate:
 
 cdef class CPU: 
     cdef np.uint8_t accumulator 
-    cdef np.uint8_t status
+    cdef np.uint8_t status_reg
     cdef np.uint8_t program_counter
+    cdef dict opcodes_map 
 
     # just for testing. write proper tests later
     cdef np.ndarray instructions
 
     def __init__(self):
         self.accumulator = 0
-        self.instructions = np.array([hex(0xa9), hex(0xc0), hex(0xaa), hex(0xe8), hex(0x00)])
+        self.instructions = np.array([hex(0xa9), hex(0x00), hex(0xaa), hex(0xe8), hex(0x00)])
+        self.status_reg = 0b0000_0000 # use only be 7 bits
+        self.opcodes_map = { 
+            "0xa9": self.LDA, 
+        }
    
     cdef str fetch(self):
         """ fetch instruction from PRG ROM """ 
         c_i = str(self.instructions[self.program_counter]) 
         return c_i
-        
+
+    def LDA(self):
+        self.accumulator = int(self.instructions[self.program_counter], 16)
+
+        """ update status register """
+
+        # set zero flag
+        if (self.accumulator == 0): 
+            self.status_reg = self.status_reg | 0b0000_0010
+        else: 
+            self.status_reg = self.status_reg & 0b1111_1101
+
+        # set negative flag 
+        if (self.accumulator & 0b1000_0000 != 0):
+            self.status_reg = self.status_reg | 0b1000_0000
+        else:
+            self.status_reg = self.status_reg & 0b0111_1111
+
+        # print(f"Status Bit {bin(self.status_reg)}")
+        return 1 
+
     def execute(self): 
         for i in range(len(self.instructions)): # switch to while loop once "BREAK" is implemented
-            c_i = self.fetch()
-            self.program_counter += 1
-            
-            """ TODO : check and execute current instruction """ 
 
-            print(c_i)
+            c_i = hex(int(self.fetch(), 16))
+
+            self.program_counter += 1
+
+            op = self.opcodes_map.get(c_i, lambda: "Invalid Opcode")()
+
 
